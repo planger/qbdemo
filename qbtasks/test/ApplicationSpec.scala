c4c29158ba752fbdceb16f99d5266b35559c50c7
Philip Langer <planger@eclipsesource.com>
2014-10-24 12:18:37 +0200

    Add tests for RESTful API

--- qbtasks/test/ApplicationSpec.scala
+++ qbtasks/test/ApplicationSpec.scala
@@ -1,30 +1,84 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
-
 import play.api.test._
 import play.api.test.Helpers._
+import play.api.libs.json.Json
 
 /**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
+ * Tests the application's RESTful API.
  */
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
 
-  "Application" should {
+  "RESTful API" should {
 
-    "send 404 on a bad request" in new WithApplication{
-      route(FakeRequest(GET, "/boum")) must beNone
+    "reply with BAD_REQUEST on posting user with invalid email" in new WithApplication {
+      val request = FakeRequest(POST, "/users").withJsonBody(userWithInvalidEmail)
+      val response = route(request).get
+      status(response) must equalTo(BAD_REQUEST)
+    }
+    
+    "reply with BAD_REQUEST on posting user with missing email" in new WithApplication {
+      val request = FakeRequest(POST, "/users").withJsonBody(userWithMissingEmail)
+      val response = route(request).get
+      status(response) must equalTo(BAD_REQUEST)
+    }
+    
+    "reply with BAD_REQUEST on posting user with invalid status" in new WithApplication {
+      val request = FakeRequest(POST, "/users").withJsonBody(userWithInvalidStatus)
+      val response = route(request).get
+      status(response) must equalTo(BAD_REQUEST)
     }
 
-    "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
-
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+    "reply with OK on posting user without id" in new WithApplication {
+      val request = FakeRequest(POST, "/users").withJsonBody(validUser)
+      val response = route(request).get
+      status(response) must equalTo(OK)
     }
+
+    "reply with added user on getting all users after adding one" in new WithApplication {
+      val postRequest = FakeRequest(POST, "/users").withJsonBody(validUser)
+      val postResponse = route(postRequest).get
+      status(postResponse) must equalTo(OK)
+      
+      val getRequest = FakeRequest(GET, "/users").withJsonBody(validUser)
+      val getResponse = route(getRequest).get
+      status(getResponse) must equalTo(OK)
+      contentAsString(getResponse) must contain("Bart Simpson")
+    }
+
   }
+
+  private val validUser = Json.parse("""
+      {
+		  "name": "Bart Simpson",
+		  "status": "ACTIVE",
+		  "email": "bart@simpson.com"
+      }
+  """)
+
+  private val userWithInvalidEmail = Json.parse("""
+      {
+		  "name": "Bart Simpson",
+		  "status": "ACTIVE",
+		  "email": "wrong"
+      }
+  """)
+  
+  private val userWithMissingEmail = Json.parse("""
+      {
+		  "name": "Bart Simpson",
+		  "status": "ACTIVE"
+      }
+  """)
+  
+  private val userWithInvalidStatus = Json.parse("""
+      {
+		  "name": "Bart Simpson",
+		  "status": "WRONG",
+		  "email": "bart@simpson.com"
+      }
+  """)
+
 }
